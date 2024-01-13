1d948659284cbbd9e7d214b32dc62919c049a0fa
Nicholas <nsheppard999@gmail.com>
2024-01-13 14:52:26 -0400

    Finishing exercise 5
    Got the first few tests done no problem.  The last 2 tests however I had
    a lot of issues with and no amount of googling found me the function i
    was looking for.  One peek in the solutions and I found !Number.isInteger
    and it was exactly what I was looking for.  I plugged it in and it worked
    perfectly

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let array = [];
+    let sum = 0;
 
+    if(!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR"
+    if (start < 0 || end < 0) return "ERROR";
+    
+    else if (start < end){
+
+        for (let i = start; i <= end; i++) {
+            array.push(i);
+        }
+
+    }
+    
+    else if (end < start) {
+        for (let i = start; i >= end; i--) {
+            array.push(i);
+            console.log(array[i]);
+        }
+    }
+
+    for (let i = 0; i < array.length; i++){
+        sum += array[i];
+        
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
