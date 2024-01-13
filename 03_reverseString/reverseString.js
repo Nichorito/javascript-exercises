20b5f09cc5080ce1c1e20d5747d823d8d7db7c9b
Nicholas <nsheppard999@gmail.com>
2024-01-13 13:30:49 -0400

    Finishing the exercise
    Nothing crazy to comment about here, just used some arrays and array
    functions to split a string into characters and then rejoined them
    as a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringArray = string.split('');
+    let reverse = '';
 
+    for (let i = stringArray.length - 1; i >= 0; i--){
+        reverse += stringArray[i];
+    }
+
+    return reverse;
 };
 
 // Do not edit below this line
