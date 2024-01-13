4879de21881e73c35927044ffe2a3b939434e858
Nicholas <nsheppard999@gmail.com>
2024-01-12 21:12:21 -0400

    Finishing Test 1
    Was able to pass test 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    string = 'hey';
+    num = 3;
+    
+    let repeated = '';
 
+    for (i = 0; i < num; i++) {
+        repeated += string; 
+        console.log(repeated);
+    }
+    return repeated;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
