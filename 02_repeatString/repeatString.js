ab7d0067c7101357917f2f1487df5baa233d93c0
Nicholas <nsheppard999@gmail.com>
2024-01-13 12:45:31 -0400

    Making 'repeatString' repeat from prompt
    Added a paragraph to be updated to the final message
    by prompting the user for a number of times to repeat
    'hey'

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,17 +1,26 @@
-const repeatString = function(string, num) {
-    string = 'hey';
-    num = 3;
+document.addEventListener('DOMContentLoaded', function() {
+
     
-    let repeated = '';
+    for (num = 0; num != 0;) {
+        let string = 'hey';
+        num = prompt("How many times would you like to repeat 'hey'?", 0)
+        let elem = document.getElementById("repeatedWord");
 
-    for (i = 0; i < num; i++) {
-        repeated += string; 
-        console.log(repeated);
+        let finalString = repeatString(string, num);
+
+        function repeatString(string, num) { 
+            let repeated = '';
+
+            for (let i = 0; i < num; i++) {
+                repeated += string; 
+                console.log(repeated);
+            }
+            return repeated;
+        }
+
+        console.log(finalString);
+
+        // Set the text content inside the event listener
+        elem.textContent = finalString;
     }
-    return repeated;
-};
-
-// Do not edit below this line
-module.exports = repeatString;
-
-
+});
