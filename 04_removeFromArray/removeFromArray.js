5b2ab5d662083ded022ecf3d575509d32ac55b23
Nicholas <nsheppard999@gmail.com>
2024-01-13 14:11:12 -0400

    Finishing exercise 4
    Had to use a lot of help from google for this one.  I learned about how
    to use the indexOf function and splice function properly

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
 
+    toRemove.forEach((x) =>{
+        const index = array.indexOf(x);
+        
+        if (index > -1) {
+        array.splice(index, 1);
+        }
+    })
+
+    return array;     
 };
 
 // Do not edit below this line
