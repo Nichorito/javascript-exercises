69c9487a139a77a09094df19ad4538956b4ed2fb
Nicholas <nsheppard999@gmail.com>
2024-01-13 13:00:55 -0400

    Fixing everything
    Figured out that I understood the problem wrong, I didn't have to pass
    any variables from my code into the function repeatString(), since
    the spec file already had all the function calls in it.
    
    Will keep this in mind for future assignments

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,26 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-    
-    for (num = 0; num != 0;) {
-        let string = 'hey';
-        num = prompt("How many times would you like to repeat 'hey'?", 0)
-        let elem = document.getElementById("repeatedWord");
-
-        let finalString = repeatString(string, num);
-
-        function repeatString(string, num) { 
-            let repeated = '';
-
-            for (let i = 0; i < num; i++) {
-                repeated += string; 
-                console.log(repeated);
-            }
-            return repeated;
-        }
-
-        console.log(finalString);
-
-        // Set the text content inside the event listener
-        elem.textContent = finalString;
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let repeated = '';
+    for (i = 0; i < num; i++) {
+        repeated += string; 
+        console.log(repeated);
     }
-});
+    return repeated;
+};
+
+// Do not edit below this line
+module.exports = repeatString;
