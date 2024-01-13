952da70be5c100920f4277068b05a2c3d236dd95
Nicholas <nsheppard999@gmail.com>
2024-01-13 15:17:31 -0400

    Finishing exercise 6
    Didn't have a lot of trouble with this one, just used some simple if else
    loops to return true or false.  After looking at the solution however I
    realized the solution I provided could be easily simplified to remove the
    if else loops

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+//if the remainder = 0 after the year is divided by 4 then return true,
+    //otherwise return false
 
+const leapYears = function(year) {
+
+    if (year % 100 === 0 && !(year % 400 === 0)) {
+        return false;
+    }
+    else if (year % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+    
 };
 
 // Do not edit below this line
